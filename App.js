f662c7424248a266eace8de51ce913bbbd9ea4d8
denis137255 <paulenkadenis@gmail.com>
2023-08-09 19:10:28 +0200

    Authentification success

--- App.js
+++ App.js
@@ -1,9 +1,23 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
-import React from 'react';
+import React, {useEffect, useState} from 'react';
 import LoginScreen from './src/screens/Auth/Login/LoginScreen';
+import Menu from './src/screens/MainMenu/Menu';
+import {User, onAuthStateChanged} from 'firebase/auth';
+import { FIREBASE_AUTH } from './src/firebase/FirebaseConfig';
 
 const Stack = createNativeStackNavigator();
+
+const InsideStack = createNativeStackNavigator();
+
+//dodaj screen za sve
+function InsideLayout() {
+  return (
+    <InsideStack.Navigator>
+      <InsideStack.Screen name="Menu" component={Menu} />
+    </InsideStack.Navigator>
+  );
+}
 
 export default function App() {
   return (
--- src/screens/Auth/Login/LoginScreen.js
+++ src/screens/Auth/Login/LoginScreen.js
@@ -1,12 +1,62 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import { View, Text, StyleSheet, TextInput, Button, ActivityIndicator, KeyboardAvoidingView} from 'react-native'
+import React, {useState} from 'react'
+import { FIREBASE_AUTH } from '../../../firebase/FirebaseConfig';
+import { async } from '@firebase/util';
+import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from 'firebase/auth';
 
 const LoginScreen = () => {
+    const [email,setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [loading, setLoading] = useState(false);
+    const auth = FIREBASE_AUTH;
+
+const signIn = async () => {
+    setLoading(true);
+    try {
+        const response = await signInWithEmailAndPassword(auth,email,password);
+        console.log(response);
+    } catch(error) {
+        console.log(error)
+        alert('Sign in failed: ' + error.message);
+    } finally{
+        setLoading(false);
+    }
+}
+
   return (
-    <View>
-      <Text>LoginScreen</Text>
+    <View style={styles.container}>
+        <KeyboardAvoidingView behavior='padding'>
+        <TextInput value={email} style={styles.input} placeholder='Email' autoCapitalize='none' onChangeText={(text) => setEmail(text)}></TextInput>
+        <TextInput value={password} secureTextEntry={true} style={styles.input} placeholder='Password' autoCapitalize='none' onChangeText={(text) => setPassword(text)}></TextInput>
+
+        {loading ? (
+            <ActivityIndicator size="large" color="#000ff"/>
+        ) : (
+        <>
+            <Button title='Login' onPress={() => signIn()}/>
+        </>
+        )}
+    </KeyboardAvoidingView>
     </View>
   )
 }
 
 export default LoginScreen
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#fff',
+  },
+  input: {
+    width: '80%',
+    height: 40,
+    borderWidth: 1,
+    borderColor: 'gray',
+    paddingHorizontal: 10,
+    marginBottom: 20,
+  },
+});
+
