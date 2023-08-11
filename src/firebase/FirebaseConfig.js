f77a12b707e42c31d84427fdc47df319a1b75db6
denis137255 <paulenkadenis@gmail.com>
2023-08-11 18:49:58 +0200

    added Add Club function, updated Firebase

--- src/firebase/FirebaseConfig.js
+++ src/firebase/FirebaseConfig.js
@@ -1,5 +1,8 @@
 import { initializeApp } from "firebase/app";
+import { getFirestore} from 'firebase/firestore';
 import { getAuth} from 'firebase/auth'; // Import Auth related functions
+
+
 
 const firebaseConfig = {
     apiKey: "AIzaSyDe7LT6k3TZVENTEv3xkX08J0vfvNd5y_U",
@@ -13,4 +16,6 @@ const firebaseConfig = {
   
   export const FIREBASE_APP = initializeApp(firebaseConfig);
   export const FIREBASE_AUTH = getAuth(FIREBASE_APP); // Get the Auth instance
+  export const FIRESTORE_INSTANCE = getFirestore(FIREBASE_APP);
+
   
--- src/screens/Club/AddClubScreen.js
+++ src/screens/Club/AddClubScreen.js
@@ -1,67 +1,77 @@
 import React, { useState } from 'react';
-import { View, Text, TextInput, Button } from 'react-native';
+import { View, Text, TextInput, Button, Alert } from 'react-native';
+import { collection, addDoc } from 'firebase/firestore';
+import { FIRESTORE_INSTANCE } from '../../firebase/FirebaseConfig';
+
 
 // You can import additional libraries for Google Maps integration here
     
-const AddClubScreen = ( {navigation} ) =>  {
-  const [clubName, setClubName] = useState('');
-  const [location, setLocation] = useState('');
-  const [contact, setContact] = useState('');
-  const [workingHours, setWorkingHours] = useState('');
-  const [clubImage, setClubImage] = useState('');
-
-  const handleAddClub = () => {
-    navigation.navigate('AddFloor');
+const AddClubScreen = ({ navigation }) => {
+    const [clubName, setClubName] = useState('');
+    const [location, setLocation] = useState('');
+    const [contact, setContact] = useState('');
+    const [workingHours, setWorkingHours] = useState('');
+  
+    const handleAddClub = async () => {
+      if (!clubName || !location || !contact || !workingHours) {
+        Alert.alert('Missing Information', 'Please fill in all fields');
+        return;
+      }
+  
+      try {
+        const clubsCollectionRef = collection(FIRESTORE_INSTANCE, 'clubs');
+        await addDoc(clubsCollectionRef, {
+          clubName,
+          location,
+          contact,
+          workingHours,
+        });
+  
+        // Success, navigate to the next screen or perform any other action
+        navigation.replace('AddFloor'); // Replace with your desired navigation
+      } catch (error) {
+        console.error('Error adding club:', error);
+        Alert.alert('Error', 'An error occurred while adding the club');
+      }
+    };
+  
+    return (
+      <View style={{ flex: 1, padding: 16 }}>
+        <Text>Club Name:</Text>
+        <TextInput
+          value={clubName}
+          onChangeText={setClubName}
+          placeholder="Enter club name"
+          style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
+        />
+  
+        <Text>Location:</Text>
+        <TextInput
+          value={location}
+          onChangeText={setLocation}
+          placeholder="Enter location"
+          style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
+        />
+  
+        <Text>Contact:</Text>
+        <TextInput
+          value={contact}
+          onChangeText={setContact}
+          placeholder="Enter contact information"
+          style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
+        />
+  
+        <Text>Working Hours:</Text>
+        <TextInput
+          value={workingHours}
+          onChangeText={setWorkingHours}
+          placeholder="Enter working hours"
+          style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
+        />
+  
+        <Button title="Submit" onPress={handleAddClub} />
+      </View>
+    );
   };
-
-  return (
-    <View style={{ flex: 1, padding: 16 }}>
-      <Text>Club Name:</Text>
-      <TextInput
-        value={clubName}
-        onChangeText={setClubName}
-        placeholder="Enter club name"
-        style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
-      />
-
-      <Text>Location:</Text>
-      {/* Implement Google Maps integration here */}
-      <TextInput
-        value={location}
-        onChangeText={setLocation}
-        placeholder="Enter location"
-        style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
-      />
-
-      <Text>Contact:</Text>
-      <TextInput
-        value={contact}
-        onChangeText={setContact}
-        placeholder="Enter contact information"
-        style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
-      />
-
-      <Text>Working Hours:</Text>
-      {/* Implement a cool working hours picker here */}
-      <TextInput
-        value={workingHours}
-        onChangeText={setWorkingHours}
-        placeholder="Enter working hours"
-        style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
-      />
-
-      <Text>Club Image (optional):</Text>
-      {/* Implement image upload here */}
-      <TextInput
-        value={clubImage}
-        onChangeText={setClubImage}
-        placeholder="Enter image URL"
-        style={{ borderWidth: 1, padding: 8, marginBottom: 16 }}
-      />
-
-      <Button title="Next" onPress={handleAddClub}/>
-    </View>
-  );
-};
-
-export default AddClubScreen;
+  
+  export default AddClubScreen;
